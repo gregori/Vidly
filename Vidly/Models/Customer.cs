d12203e2960ca4072d948cee352fad732fcfeda6
Rodrigo Gregori <spam@gregori.eti.br>
2017-07-19 22:32:30 -0300

    Add membershipp type

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,10 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        // Navigation property
+        public MembershipType MembershipType { get; set; }
+        // Foreign key
+        public byte MembershipTypeId { get; set; }
     }
 }

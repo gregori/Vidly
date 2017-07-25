a332a7a3d60f26299723059bc67bd972da22dedd
Rodrigo Gregori <spam@gregori.eti.br>
2017-07-25 18:47:42 -0300

    Add client-side validation

--- Vidly/Models/Min18IfAMember.cs
+++ Vidly/Models/Min18IfAMember.cs
@@ -11,7 +11,8 @@ namespace Vidly.Models
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var customer = (Customer) validationContext.ObjectInstance;
-            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            if (customer.MembershipTypeId == MembershipType.Unknown 
+                || customer.MembershipTypeId == MembershipType.PayAsYouGo)
                 return ValidationResult.Success;
 
             if (customer.BirthDay == null)

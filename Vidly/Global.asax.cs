53f8971d0329407ba8a85e5a53077da459ee1d08
Rodrigo Gregori <spam@gregori.eti.br>
2017-07-27 10:40:50 -0300

    Add Customers API

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
 
 namespace Vidly
 {
@@ -12,6 +14,7 @@ namespace Vidly
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
